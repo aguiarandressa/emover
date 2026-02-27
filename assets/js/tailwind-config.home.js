c138d8a3a6a4f0435263fc21bde7c364b1bcf7d3
aguiarandressa <aguiar.andressa@gmail.com>
2026-02-27 17:10:41 -0300

    Create tailwind-config.home.js

---
+++ assets/js/tailwind-config.home.js
@@ -0,0 +1,17 @@
+/**
+ * Tema da Home (index).
+ */
+tailwind.config = window.buildTailwindConfig({
+  colors: {
+    primary: '#4fb3bf',
+    accent: '#f17c55',
+    'background-light': '#f8fafb',
+    'background-dark': '#0f172a',
+  },
+  borderRadius: {
+    DEFAULT: '0.5rem',
+    lg: '0.75rem',
+    xl: '1rem',
+    full: '9999px',
+  },
+});
